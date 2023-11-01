270d8934ea2b19b3e073a9a54b0ce041d1d3c5b5
jashan-bhayana <jashanbhayana5@gmail.com>
2023-11-01 20:08:21 +0530

    celebrity-card-update

--- clientComponents/celebrityCard.tsx
+++ clientComponents/celebrityCard.tsx
@@ -84,6 +84,77 @@ const CELEBRITYCARD = () => {
           ))}
         </Swiper>
       </div>
+      <div className="w-full flex md:hidden justify-center items-center px-[20px] ">
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={20}
+          pagination={{
+            dynamicBullets: true,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {previousCelebrities.map((celebrityDetails, index) => (
+            <SwiperSlide key={index} className="pb-[30px]">
+              <div className="w-full flex justify-center">
+                <div>
+                  <div>
+                    <Image
+                      src={celebrityDetails.celebrityPhoto}
+                      alt={index.toString()}
+                      className="w-[250px] aspect-square object-cover rounded-full "
+                    />
+                  </div>
+                  <div className="text-center mt-[10px]">
+                    <span>{celebrityDetails.celebrityName}</span>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="w-full flex md:hidden justify-center items-center px-[20px] ">
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={20}
+          pagination={{
+            dynamicBullets: true,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {previousCelebrities.map((celebrityDetails, index) => (
+            <SwiperSlide key={index} className="pb-[30px]">
+              <div className="w-full flex justify-center">
+                <div>
+                  <div>
+                    <Image
+                      src={celebrityDetails.celebrityPhoto}
+                      alt={index.toString()}
+                      className="w-[250px] aspect-square object-cover rounded-full "
+                    />
+                  </div>
+                  <div className="text-center mt-[10px]">
+                    <span>{celebrityDetails.celebrityName}</span>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      
     </>
   );
 };
