01566776a36a716f2ba12774436a52006e61c66c
Prashant-S29 <prashant.s2922@gmail.com>
2023-12-03 00:29:52 +0530

    cursor error fixed

--- clientComponents/cursor.tsx
+++ clientComponents/cursor.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { Cursor } from "react-creative-cursor";
 import "react-creative-cursor/dist/styles.css";
 
-import AnimatedCursor from "react-animated-cursor";
-
-import { motion } from "framer-motion";
 
 const CURSOR = () => {
   return (
@@ -21,14 +18,6 @@ const CURSOR = () => {
           textAnimationDuration={1}
         />{" "}
       </div>
-      {/* <AnimatedCursor
-        innerSize={8}
-        outerSize={8}
-        color='193, 11, 111'
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={5}
-      /> */}
     </>
   );
 };
