293d625997f991dbfa7198865c02cc928d8cb3d2
Prashant-S29 <prashant.s2922@gmail.com>
2023-12-03 20:30:12 +0530

    db removed. Now api fetching data directly from admin panel

--- app/api/events/route.js
+++
@@ -1,9 +0,0 @@
-import connectMongoDB from "../../../libs/mongodb"
-import pravah24_DB from "../../../model/event"
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    await connectMongoDB();
-    const pravahEventsList = await pravah24_DB.find();
-    return NextResponse.json({ pravahEventsList })
-}
