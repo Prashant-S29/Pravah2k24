c7dcb11d0298fb6f95ed324dc338c1ba28f1fb80
Prashant-S29 <prashant.s2922@gmail.com>
2023-12-03 20:14:58 +0530

    apis again set to server side fetching

--- components/eventList.tsx
+++ components/eventList.tsx
@@ -1,85 +1,11 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-
-import { loader } from "@/public";
+import React from "react";
 import EVENT_REG_CARDS from "@/clientComponents/eventRegCard";
-
-// const getEventDetails = async () => {
-//   try {
-//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}`, {
-//       cache: "no-cache",
-//     });
-//     const allEventList = res.json();
-
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch events");
-//     }
-//     return allEventList;
-//   } catch (error) {
-//     console.log("Error:  ", error);
-//   }
-// };
 
 import { getEventDetails } from "@/fetchData";
 
-const EVENTLIST = () => {
-  const [errorMessageDisplay, setErrorMessageDisplay] = useState({
-    display: false,
-    message: "",
-  });
-  const [successMessageDisplay, setSuccessMessageDisplay] = useState({
-    display: false,
-    message: "",
-  });
-  const [isLoading, setIsLoading] = useState(false);
-  const [eventData, setEventData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_ENDPOINT}`,
-          {
-            cache: "no-cache",
-          }
-        );
-
-        if (!response.ok) {
-          setErrorMessageDisplay({
-            display: true,
-            message: "Failed to fetch events. Unstable network detected!",
-          });
-
-          setTimeout(() => {
-            setErrorMessageDisplay({ display: false, message: "" });
-          }, 2000);
-          throw new Error("Network response was not ok.");
-        }
-
-        const data = await response.json();
-        console.log(data);
-        setEventData(data);
-        setIsLoading(false);
-      } catch (error) {
-        setErrorMessageDisplay({
-          display: true,
-          message: "Failed to fetch events. Try again",
-        });
-
-        setTimeout(() => {
-          setErrorMessageDisplay({ display: false, message: "" });
-        }, 2000);
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+const EVENTLIST = async () => {
+  const eventData = await getEventDetails();
+  // console.log("eventDetails", eventData);
   return (
     <>
       <div className=" px-[20px] min-h-[80vh] flex gap-5 flex-wrap justify-center my-[20px]">
@@ -120,29 +46,7 @@ const EVENTLIST = () => {
           </div>
         )}
 
-        {isLoading && (
-          <div className="w-full min-h-[70vh] flex justify-center items-center">
-            <div className="flex justify-center animate-spin ">
-              <Image src={loader} alt="loader" className="w-[50px]" />
-            </div>
-          </div>
-        )}
       </div>
-      {errorMessageDisplay["display"] && (
-        <div className=" w-[calc(100%-40px)] sm:w-fit text-center fixed left-[50%] z-[48] rounded-[10px] -translate-x-[50%] bottom-0 my-[20px] px-[10px] py-[7px] bg-red-500">
-          <span className="font-semibold text-[12px] sm:text-[13px] text-white">
-            {errorMessageDisplay["message"]}
-          </span>
-        </div>
-      )}
-
-      {successMessageDisplay["display"] && (
-        <div className=" w-[calc(100%-40px)] sm:w-fit text-center fixed left-[50%] z-[48] rounded-[10px] -translate-x-[50%] bottom-0 my-[20px] px-[10px] py-[7px] bg-green-500 ">
-          <span className="font-semibold text-[12px] sm:text-[13px] text-white">
-            {successMessageDisplay["message"]}
-          </span>
-        </div>
-      )}
     </>
   );
 };
