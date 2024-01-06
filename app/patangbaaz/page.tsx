67873c10df0d9ebd750f73506587b0b110b79081
Prashant-S29 <prashant.s2922@gmail.com>
2024-01-06 12:34:59 +0530

    patangbazz event added

---
+++ app/patangbaaz/page.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const PATANBAAZ = () => {
+  return (
+    <>
+      <div className="w-full min-h-screen block sm:flex justify-center gap-[50px] lg:gap-[100px] px-[20px]">
+        <div className="flex justify-center">
+          <div className="px-[20px] ">
+            <Image
+              src="https://res.cloudinary.com/denl2iizs/image/upload/v1704521957/patanbaazEvent_iafxxk.jpg"
+              alt="patanbazzEvent"
+              width={350}
+              height={500}
+              className="object-cover  w-[250px] sm:w-[300px] lg:w-[350px] rounded-[20px] shadow-xl"
+            />
+          </div>
+        </div>
+        <div className="mt-[10px] text-center">
+          <div className="leading-none">
+            <span className="font-black  text-[32px] sm:text-[38px] md:text-[52px] lg:text-[64px]">
+              Patangbaaz
+            </span>
+          </div>
+          <div>
+            <span className="font-medium text-[13px] sm:text-[17px]">
+              Kite Flying Competition
+            </span>
+          </div>
+          <div className="mt-[20px] font-semibold  text-[16px] sm:text-[18px]">
+            <span>Date: January 13, 2023</span>
+          </div>
+          <div className=" font-semibold  text-[16px] sm:text-[18px]">
+            <span>Time: 01:30 PM Onwards</span>
+          </div>
+          <div className=" font-semibold  text-[16px] sm:text-[18px]">
+            <span>Venue: Football Ground, SKIT</span>
+          </div>
+          <div className=" font-semibold  text-[16px] sm:text-[18px]">
+            <span>Registration Fee: ₹60/- per team</span>
+          </div>
+          {/* <div className="flex justify-center my-[15px]">
+            <div className="p-[10px] bg-white shadow-lg w-fit rounded-[15px]">
+              <div>
+                <Image
+                  src="https://res.cloudinary.com/denl2iizs/image/upload/v1704523176/patanbazzQR_hwzvht.png"
+                  alt="patanbazzEventQR"
+                  width={120}
+                  height={120}
+                  className="object-cover"
+                />
+              </div>
+            </div>
+          </div>
+          <div className=" font-semibold ">
+            <span>Scan and Register !</span>
+          </div> */}
+          <div className="mt-[10px]">
+            <Link
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfxL9Asa4FDvThe-DwINPi56RnNFRmr2_mjLRbzgBVWn5Ft_Q/viewform"
+              target="_blank"
+            >
+              <button className="px-[15px] py-[8px] bg-black text-white text-[14px] font-semibold rounded-[10px]">
+                Register Now
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PATANBAAZ;
--- components/latestNews.tsx
+++ components/latestNews.tsx
@@ -9,12 +9,12 @@ const LATESTNEWS = () => {
       <div className="z-40 sticky top-0 w-full py-[5px] bg-yellow-300 font-semibold">
         <div className="hidden md:block">
           <Marquee autoFill={true}>
-            <Link href="https://forms.gle/1sAvMsYrGm9M7zpm9" target="blank">
+            <Link href="/patangbaaz">
               <div className="px-[30px] text-[14px] sm:text-[16px]">
                 <span>
-                  Event suggestion form is live.{" "}
+                  Patangbaaz event is Live !!.{" "}
                   <span className="underline font-bold">Click here</span> to
-                  suggest your favourite events for Pravah 2024.
+                  participate & witness it live at SKIT.
                 </span>
               </div>
             </Link>
@@ -22,12 +22,12 @@ const LATESTNEWS = () => {
         </div>
         <div className="block md:hidden">
           <Marquee autoFill={true} speed={40}>
-            <Link href="https://forms.gle/1sAvMsYrGm9M7zpm9" target="blank">
+            <Link href="/patangbaaz">
               <div className="px-[30px] text-[14px] sm:text-[16px]">
                 <span>
-                  Event suggestion form is live.{" "}
+                  Patangbaaz event is Live !!.{" "}
                   <span className="underline font-bold">Click here</span> to
-                  suggest your favourite events for Pravah 2024.
+                  participate & witness it live at SKIT.
                 </span>
               </div>
             </Link>
