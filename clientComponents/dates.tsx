3e85508c6626e9ff0fb2dafb0b6442debc885c15
Prashant-S29 <prashant.s2922@gmail.com>
2024-02-01 15:52:04 +0530

    date format updated

--- clientComponents/dates.tsx
+++ clientComponents/dates.tsx
@@ -5,7 +5,7 @@ const DATES = () => {
     <>
       <div className="mt-5">
         <span className="text-[24px] md:text-[36px] lg:text-[42px] font-black leading-tight">
-          11 March{"'"}24 - 16 March{"'"}24
+          11 - 16 March 2024
         </span>
       </div>
     </>
